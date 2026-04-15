ac9a0d8ad7512fbea54d63ad39a270a0b2b5c42e
Manjunatha H <manju.h5355@gmail.com>
2026-04-15 23:41:06 +0530

    Myfirefox file added

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,19 +1,17 @@
 package com.example;
 
-import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.*;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WindowType;
 
 public class App {
     public static void main(String[] args) {
 
-        // Set GeckoDriver path
         System.setProperty("webdriver.gecko.driver", "drivers/geckodriver");
 
-        // Headless options for Jenkins
         FirefoxOptions options = new FirefoxOptions();
+
+        // IMPORTANT: let Selenium find Firefox automatically
         options.addArguments("--headless");
         options.addArguments("--no-sandbox");
         options.addArguments("--disable-dev-shm-usage");
@@ -21,24 +19,19 @@ public class App {
         WebDriver driver = new FirefoxDriver(options);
 
         try {
-            // First site
             driver.get("https://www.saucedemo.com/");
             driver.findElement(By.id("user-name")).sendKeys("standard_user");
             driver.findElement(By.id("password")).sendKeys("secret_sauce");
             driver.findElement(By.id("login-button")).click();
 
-            // New tab 1
             driver.switchTo().newWindow(WindowType.TAB);
             driver.get("https://www.automationexercise.com/");
 
-            // New tab 2
             driver.switchTo().newWindow(WindowType.TAB);
             driver.get("https://practicetestautomation.com/practice-test-login/");
             driver.findElement(By.id("username")).sendKeys("student");
             driver.findElement(By.id("password")).sendKeys("Password123");
             driver.findElement(By.id("submit")).click();
-
-            System.out.println("Execution Successful");
 
         } finally {
             driver.quit();
